a6c49415340200737fa5dabb3085cc68eef6382a
AJITHKUMAR <116996938+ajithkumar-kasi@users.noreply.github.com>
2022-11-07 15:58:33 +0530

    primitive data types

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
-let name = "ajith";
-let msg=("My Name is " + name + "kumar");
-alert(msg);
+let FirstName="Ajith";//String
+
+let age=21;//integer
+
+let Areyouvirgin=true;//boolean
+
+let LastName=undefined;//undefined
+
+let tropy=null;//null
+
+alert(FirstName);
+alert(age);
+alert(LastName);
+alert(Areyouvirgin);
+alert(tropy);
