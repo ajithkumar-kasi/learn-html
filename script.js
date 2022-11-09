9fccce8eeff8494481ea55b3b1ba4fbf1998de4e
AJITHKUMAR <116996938+ajithkumar-kasi@users.noreply.github.com>
2022-11-09 11:48:22 +0530

    items

--- script.js
+++ script.js
@@ -1,16 +1,3 @@
-let savingamount=0;
+const items=["apple","lemon","green","orange"]
 
-if(savingamount>10000){
-    console.log("Buy Anroid Mobile");
-}
-else if(savingamount>60000){
-    console.log("Buy Iphone Mobile");
-}
-else if(5000 >10000){
-    console.log("Buy Baic Mobile");
-}
-else{
-    console.log("You can't afford mobile phone now");
-}
-
-
+console.log(items.indexOf("yellow"));
