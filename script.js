af8cfd01148678c653a40bb535644c8589354521
AJITHKUMAR <116996938+ajithkumar-kasi@users.noreply.github.com>
2022-11-10 20:35:50 +0530

    array

--- script.js
+++ script.js
@@ -1,12 +1,5 @@
-const persons=[
-    {id:1,first_name:"ajith",last_name:"kumar"},
-    {id:2,first_name:"gopi",last_name:"nath"},
-    {id:3,first_name:"dhila",last_name:"haris"},
-];
-let final=persons.map(function(value){
-    return [value.first_name, value.last_name] .join(" ");
-}
-   
-);
+var array=[1,2,3,4,5];
 
-console.log(final);
+let result=array.slice();
+
+console.log(result);
