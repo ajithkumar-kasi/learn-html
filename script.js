495a21eeeabd5428440e845f91015ae7ff166eff
AJITHKUMAR <116996938+ajithkumar-kasi@users.noreply.github.com>
2022-11-13 15:11:48 +0530

    create array

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
+// Method 1
+let array=[1,2,4,5,6,8];
+console.log(array);
 
-function sumExpense(tiffen,lunch,petrol){
-    return tiffen+lunch+petrol;
-    
-};
+//Method 2
+let array2=[];
+array2[1]="Name";
+array2[0]=56;
+console.log(array2);
 
-let result=sumExpense(50,100,99);
-console.log("your total expense is:"+result);  
+
+//Method 3
+let array3=new Array("Name",15,76);
+
+console.log(array3);
