5955f034187aa48e9a7e063501cda50f700ca22b
AJITHKUMAR <116996938+ajithkumar-kasi@users.noreply.github.com>
2022-11-13 15:50:49 +0530

    empty the array

--- script.js
+++ script.js
@@ -1,15 +1,14 @@
-// Method 1
-let array=[1,2,4,5,6,8];
+var array=[1,2,3,4,5,6];
+array=[];
+console.log(array);
+//method 2
+var array=[1,2,3,4,5,6];
+array.length=0;
 console.log(array);
 
-//Method 2
-let array2=[];
-array2[1]="Name";
-array2[0]=56;
-console.log(array2);
-
-
-//Method 3
-let array3=new Array("Name",15,76);
-
-console.log(array3);
+//method 3
+var array=[1,2,3,4,5,6];
+while(array.length){
+array.pop();
+}
+console.log(array);
