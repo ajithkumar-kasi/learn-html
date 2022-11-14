1ce6190dec62309306408e452c2c0f230f0aa727
AJITHKUMAR <116996938+ajithkumar-kasi@users.noreply.github.com>
2022-11-14 09:55:41 +0530

    is array

--- script.js
+++ script.js
@@ -1,14 +1,29 @@
-var array=[1,2,3,4,5,6];
-array=[];
-console.log(array);
-//method 2
-var array=[1,2,3,4,5,6];
-array.length=0;
-console.log(array);
+var array=[1,2,34,5,6];
+let obj={id:2};
+let other=undefined;
+let string="Anbu";
+// method 1
+//let result=Array.isArray(obj);
+// console.log(result);
 
-//method 3
-var array=[1,2,3,4,5,6];
-while(array.length){
-array.pop();
+//method 2
+function check(val){
+    if (typeof val =="string"){
+        console.log("It is an string");
+    }
+    else if (typeof val == "obj"){
+        console.log("It is an object");
+    }
+    else if (typeof val == "other"){
+        console.log("It is an undefined");
+    }
+    else if (typeof val == "number"){
+        console.log("It is an number");
+    }
+    else {
+        console.log("It is an array");
+    }
 }
-console.log(array);
+check(string);
+
+
