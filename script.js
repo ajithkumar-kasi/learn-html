57279be1ac39ddb4f84052ab5c7cc5ee30360861
AJITHKUMAR <116996938+ajithkumar-kasi@users.noreply.github.com>
2022-11-14 09:58:13 +0530

    push the value an end of the array

--- script.js
+++ script.js
@@ -1,29 +1,4 @@
 var array=[1,2,34,5,6];
-let obj={id:2};
-let other=undefined;
-let string="Anbu";
-// method 1
-//let result=Array.isArray(obj);
-// console.log(result);
-
-//method 2
-function check(val){
-    if (typeof val =="string"){
-        console.log("It is an string");
-    }
-    else if (typeof val == "obj"){
-        console.log("It is an object");
-    }
-    else if (typeof val == "other"){
-        console.log("It is an undefined");
-    }
-    else if (typeof val == "number"){
-        console.log("It is an number");
-    }
-    else {
-        console.log("It is an array");
-    }
-}
-check(string);
-
+array.push(4)
+console.log(array);
 
