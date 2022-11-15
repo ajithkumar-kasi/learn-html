201f88158f6a2704af5bc937b8b18307709cd2c9
AJITHKUMAR <116996938+ajithkumar-kasi@users.noreply.github.com>
2022-11-15 11:09:38 +0530

    add method 2

--- add.js
+++ add.js
@@ -1,6 +1,6 @@
-const num1=5;
-const num2=14;
+const num1=parseInt(prompt("Enter the number:"));
+const num2=parseInt(prompt("Enter the number:"));
 
 const sum=num1 +num2;
 
-console.log(`the sum of ` + num1 +` and ` + num2 + ` is: `+sum);
+console.log(`the sum of ${num1} and ${num2} is:${sum}`);
