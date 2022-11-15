ac92527b456da7eedf024b3257a22c53e4e36863
AJITHKUMAR <116996938+ajithkumar-kasi@users.noreply.github.com>
2022-11-15 19:27:06 +0530

    positive,negative,zero

---
+++ pnz.js
@@ -0,0 +1,11 @@
+const number=prompt("enter the number");
+
+if(number >0){
+    console.log("the number is positive");
+}
+else if(number == 0){
+    console.log("the number is zero");
+}
+else{
+    console.log("the number is negative");
+}
