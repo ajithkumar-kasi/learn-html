e96adad409a5bd555ff6ec1ce582c36f61bcf5ca
AJITHKUMAR <116996938+ajithkumar-kasi@users.noreply.github.com>
2022-11-15 19:39:47 +0530

    largest number

---
+++ large.js
@@ -0,0 +1,7 @@
+const number1=prompt("enter the number");
+const number2=prompt("enter the number");
+const number3=prompt("enter the number");
+
+const largest= Math.max(number1,number2,number3);
+
+console.log(largest);
