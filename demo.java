b77f5a0c4d0dbdb91cee52c1d4185b0f80ce4728
AJITHKUMAR <116996938+ajithkumar-kasi@users.noreply.github.com>
2022-11-24 16:55:26 +0530

    string,int,float

--- demo.java
+++ demo.java
@@ -5,5 +5,19 @@ public class demo {
 		System.out.println("Enter your Name");
 		String name= scanner.nextLine();
 		System.out.println("hello"+name);
+		
+		System.out.println("enter yur age");
+		Integer age= scanner.nextInt();
+		System.out.println("your age is " + age + " right");
+		scanner.nextLine();
+		
+		System.out.println("enter the email");
+		String email= scanner.nextLine();
+		System.out.println("your email"+email);
+		
+		System.out.println("enter float value");
+		Float pi= scanner.nextFloat();
+		System.out.println("pi value is"+ pi);
+		
 		}
 }
