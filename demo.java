fa76b66dc0e617a680c4cfb6c0c812fcfc797f08
AJITHKUMAR <116996938+ajithkumar-kasi@users.noreply.github.com>
2022-11-24 16:48:13 +0530

    Add files via upload

---
+++ demo.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class demo {
+	public static void main(String args[]) {
+		Scanner scanner=new Scanner(System.in);
+		System.out.println("Enter your Name");
+		String name= scanner.nextLine();
+		System.out.println("hello"+name);
+		}
+}
