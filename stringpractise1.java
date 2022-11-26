99593a483fb38dff342af71869332af22ef3b1c8
AJITHKUMAR <116996938+ajithkumar-kasi@users.noreply.github.com>
2022-11-26 17:31:56 +0530

    Add files via upload

---
+++ stringpractise1.java
@@ -0,0 +1,16 @@
+
+public class stringpractise1 {
+
+	public static void main(String[] args) {
+		
+		
+		String str=new String("ajithkumar");
+		
+	    
+	    String str2=str.replace('a','r');
+		
+		
+		System.out.println(str2);
+		
+	}
+}
