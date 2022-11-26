ea731119ee4bbe580860d76ab98bd34a1c29cb61
AJITHKUMAR <116996938+ajithkumar-kasi@users.noreply.github.com>
2022-11-26 22:04:27 +0530

    Add files via upload

---
+++ binaryornot.java
@@ -0,0 +1,11 @@
+
+public class binaryornot {
+
+	public static void main(String[] args) {
+		
+		String d="01/12/2022";
+		System.out.println(d.matches("[0-3][0-9]/[01][0-9]/[0-9]{4}"));
+
+	}
+
+}
