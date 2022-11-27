2ede0819306b5092cd018303823c4aca3876b7a5
AJITHKUMAR <116996938+ajithkumar-kasi@users.noreply.github.com>
2022-11-27 16:31:45 +0530

    Add files via upload

---
+++ dayinwords.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+public class dayinwords {
+
+	public static void main(String[] args) {
+		int n;
+		Scanner sc=new Scanner(System.in);
+		
+		System.out.println("Enter the number:");
+		n=sc.nextInt();
+		
+		switch(n) {
+		case 1:
+			System.out.println("Monday");
+			break;
+		case 2:
+			System.out.println("Tuesday");
+			break;
+		case 3:
+		
+			System.out.println("Wednesday");
+			break;
+		case 4:
+			System.out.println("Thursday");
+			break;
+		case 5:
+			System.out.println("Friday");
+			break;
+		case 6:
+			System.out.println("saturday");
+			break;
+		case 7:
+			System.out.println("Sunday");
+			break;
+		default:
+			System.out.println("Invalid");
+		}
+
+	}
+
+}
