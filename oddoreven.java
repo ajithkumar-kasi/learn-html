b5b3045cbedfb4b924a82c0228ec58348b0c99ee
AJITHKUMAR <116996938+ajithkumar-kasi@users.noreply.github.com>
2022-11-27 12:19:23 +0530

    Add files via upload

---
+++ oddoreven.java
@@ -0,0 +1,16 @@
+
+public class oddoreven {
+
+	public static void main(String[] args) {
+		int n=21;
+		
+		if(n%2==0) {
+			System.out.println("the number is even");	
+		}
+		else {
+			System.out.println("the number is odd");
+		}
+
+	}
+
+}
