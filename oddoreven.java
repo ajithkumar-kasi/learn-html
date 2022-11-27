a9da2ec93e4a8ed4193a41c0a30fad542db586fa
AJITHKUMAR <116996938+ajithkumar-kasi@users.noreply.github.com>
2022-11-27 12:23:47 +0530

    input from user

--- oddoreven.java
+++ oddoreven.java
@@ -1,8 +1,13 @@
-
+import java.util.Scanner;
 public class oddoreven {
 
 	public static void main(String[] args) {
-		int n=21;
+		int n;
+		
+		Scanner sc= new Scanner(System.in);
+		
+		System.out.print("Enter the number: ");
+		n=sc.nextInt();
 		
 		if(n%2==0) {
 			System.out.println("the number is even");	
