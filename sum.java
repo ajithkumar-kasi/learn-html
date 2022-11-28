3a0877041dabaaf0ac0b64ef0f2555c00dc08b63
AJITHKUMAR <116996938+ajithkumar-kasi@users.noreply.github.com>
2022-11-28 20:01:55 +0530

    Add files via upload

---
+++ sum.java
@@ -0,0 +1,22 @@
+
+public class sum {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int a[]= {2,9,7,65,6,7,8};
+		int sum=0;
+		/* for each looop
+		for(int x:a)
+		{
+			sum=sum+x;
+		}*/
+		
+		for(int i=0;i<a.length;i++)
+		{
+			
+		}
+		System.out.println("The sumof elements is:"+sum);
+	}
+
+}
