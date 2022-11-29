228bbc390322b8366cfe82efae5b07640d6b3e57
AJITHKUMAR <116996938+ajithkumar-kasi@users.noreply.github.com>
2022-11-29 17:17:59 +0530

    Add files via upload

--- nestedloop.java
+++ nestedloop.java
@@ -10,6 +10,9 @@ public class nestedloop {
 			System.out.println("");
 		}*/
 
+		
+		
+		
 		/*for(int i=1;i<=5;i++) {
 			for(int j=1;j<=5;j++) {
 				System.out.print(j+" ");
@@ -17,22 +20,87 @@ public class nestedloop {
 			System.out.println("");
 		}*/
 
+		
+		
+		
 		/*for(int i=1;i<=5;i++) {
 			for(int j=1;j<=5;j++) {
 				System.out.print(i+" ");
 			}
 			System.out.println("");
 		}*/
+		
+		
 
-		int count=0;
+		/*int count=0;
 		for(int i=1;i<=5;i++) {
 			for(int j=1;j<=5;j++) {
 				count++;
 				System.out.format("%02d ",count);
 			}
 			System.out.println("");
+		}*/
+		
+		/*for(int i=1;i<=5;i++) {
+			for(int j=1;j<=i;j++) {
+				System.out.print(j+" ");
+			}
+			System.out.println("");
+		}*/
+		
+		
+		/*int count=0;
+		for(int i=1;i<=5;i++) {
+			for(int j=1;j<=i;j++) {
+				
+				count++;
+				System.out.print(count+" ");
+			}
+			System.out.println("");
+		}*/
+		
+		/*for(int i=1;i<=5;i++) {
+			for(int j=1;j<=5-i+1;j++) {
+				System.out.print(i+" ");
+			}
+			System.out.println("");
+		}*/
+		
+		
+		
+		/*for(int i=1;i<=5;i++) {
+			for(int j=1;j<=5-i+1;j++) {
+				System.out.print(j+" ");
+			}
+			System.out.println("");
+		}*/
+		
+		
+		/*for(int i=1;i<=5;i++) {
+			for(int j=1;j<=5;j++) {
+				if(j>=i) {
+					System.out.format("%d",j);	
+				}
+				else {
+					System.out.print(" ");
+				}
+				
+			}
+			System.out.println("");
+		}*/
+		
+		for(int i=1;i<=5;i++) {
+			for(int j=1;j<=5;j++) {
+				if(i+j>5) {
+					System.out.print("7");	
+				}
+				else {
+					System.out.print(" ");
+				}
+				
+			}
+			System.out.println("");
 		}
-
 	}
 
 }
