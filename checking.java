c16bfc06a14c18e942b39f3472ec7d0092c7d31f
AJITHKUMAR <116996938+ajithkumar-kasi@users.noreply.github.com>
2022-12-09 13:31:23 +0530

    Add files via upload

---
+++ checking.java
@@ -0,0 +1,15 @@
+package mypack1;
+
+public class checking {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int a=4;
+		int b=5;
+		int c=a+b;
+		
+		System.out.println(c++);
+	}
+
+}
