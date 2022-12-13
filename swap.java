00a2fbb5066dab00bef1ba6b390ca330e6b617d6
AJITHKUMAR <116996938+ajithkumar-kasi@users.noreply.github.com>
2022-12-13 20:16:06 +0530

    Add files via upload

---
+++ swap.java
@@ -0,0 +1,22 @@
+package mypack1;
+import java.util.*; 
+
+public class swap {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		        System.out.println("Enter the value of x and y");  
+		        Scanner sc = new Scanner(System.in);  
+		        /*Define variables*/  
+		        int x = sc.nextInt();  
+		        int y = sc.nextInt();  
+		        System.out.println("before swapping numbers: "+x +" "+ y);  
+		       /*Swapping*/  
+		        x = x + y;   
+		        y = x - y;   
+		        x = x - y;   
+		        System.out.println("After swapping: "+x +"  " + y);   
+		    }   
+		
+
+}
