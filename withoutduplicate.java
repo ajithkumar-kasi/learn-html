1eb869ae44e830814bc7a8a3bac62803836751b4
AJITHKUMAR <116996938+ajithkumar-kasi@users.noreply.github.com>
2022-12-14 18:53:34 +0530

    Add files via upload

---
+++ withoutduplicate.java
@@ -0,0 +1,25 @@
+package mypack1;
+
+public class withoutduplicate {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int i;
+		int a[]= {1,2,3,45,76,3,4,2,1};
+		int b[]=new int[count]
+		System.out.println("the numbers are: ");
+		for( i=0;i<a.length;i++) {
+			for(int j=i+1;j<a.length;j++) {
+				if(a[i]!=a[j]) {
+					newArray[]=a[i];
+				}
+				
+			}
+			
+		}
+		System.out.println(temp[i]);
+		
+	}
+
+}
