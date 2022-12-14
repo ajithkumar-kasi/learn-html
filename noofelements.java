cdc5f39e10d7c03a22ec0e58108eef5fb07ebe23
AJITHKUMAR <116996938+ajithkumar-kasi@users.noreply.github.com>
2022-12-14 18:57:37 +0530

    Add files via upload

---
+++ noofelements.java
@@ -0,0 +1,12 @@
+package mypack1;
+
+public class noofelements {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int a[]= {9,7,5,3,1,2,4,6,8,10};
+		System.out.println("No of elements: "+a.length);
+	}
+
+}
