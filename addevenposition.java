aed3486e7bd36585cbd660964a6f9e7c13ce0e7b
AJITHKUMAR <116996938+ajithkumar-kasi@users.noreply.github.com>
2022-12-14 18:10:57 +0530

    odd position

---
+++ addevenposition.java
@@ -0,0 +1,28 @@
+package mypack1;
+
+public class addevenposition {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int a[]= {2,3,5,67,9,10,6};
+		System.out.println("The Original Array: ");
+		for(int i=0;i<a.length;i++)
+		{
+			System.out.print(a[i]+" ");
+		}
+		System.out.println(" ");
+		
+		System.out.println("The odd positions of Array: ");
+		for(int i=0;i<a.length;i=i+2)
+		{	
+				System.out.print(a[i]+" ");	
+			
+		}
+		
+		
+		
+		
+	}
+
+}
