68180b2182413760973010dfca888135409b08d0
AJITHKUMAR <116996938+ajithkumar-kasi@users.noreply.github.com>
2022-12-18 19:55:10 +0530

    rightshift

---
+++ practice1.java
@@ -0,0 +1,19 @@
+package mypack1;
+
+public class practice1 {
+
+	public static void main(String[] args) {
+
+		/*leftShift
+		System.out.println(10<<2);
+		System.out.println(10<<4);
+		System.out.println(10<<3);
+		*/
+		
+		System.out.println(10>>2);
+		System.out.println(10>>4);
+		System.out.println(10>>3);
+		
+	}
+
+}
