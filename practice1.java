22804f3cd17d30696806af567d5f7a560ad4dc50
AJITHKUMAR <116996938+ajithkumar-kasi@users.noreply.github.com>
2022-12-18 19:56:07 +0530

    leftshift

--- practice1.java
+++ practice1.java
@@ -4,16 +4,18 @@ public class practice1 {
 
 	public static void main(String[] args) {
 
-		/*leftShift
+	
 		System.out.println(10<<2);
 		System.out.println(10<<4);
 		System.out.println(10<<3);
-		*/
 		
+		
+		/*rightshift
 		System.out.println(10>>2);
 		System.out.println(10>>4);
 		System.out.println(10>>3);
 		
+		*/
 	}
 
 }
