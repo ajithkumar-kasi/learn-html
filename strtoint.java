f42ad680609991c62569620e75a5000f4676b62c
AJITHKUMAR <116996938+ajithkumar-kasi@users.noreply.github.com>
2022-12-18 20:06:52 +0530

    Add files via upload

---
+++ strtoint.java
@@ -0,0 +1,14 @@
+package mypack1;
+
+public class strtoint {
+
+	public class StringToIntExample1{  
+		public static void main(String args[]){  
+		//Declaring String variable  
+		String s="200";  
+		//Converting String into int using Integer.parseInt()  
+		int i=Integer.parseInt(s);  
+		//Printing value of i  
+		System.out.println(i);  
+		}}  
+}
