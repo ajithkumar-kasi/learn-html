e8b486c3f42d9f5157ee626620b9e4df1d79e593
AJITHKUMAR <116996938+ajithkumar-kasi@users.noreply.github.com>
2022-12-19 16:07:07 +0530

    Add files via upload

--- asciivalue.java
+++ asciivalue.java
@@ -1,20 +1,17 @@
+package mypack1;
 
 public class asciivalue {
+	public static void main(String[] args)   
+	{  
+	// character whose ASCII value to be found  
+	char ch1 = 'a';  
+	char ch2 = 'f';  
+	// variable that stores the integer value of the character  
+	int asciivalue1 = ch1;  
+	int asciivalue2 = ch2;  
+	System.out.println("The ASCII value of " + ch1 + " is: " + asciivalue1);  
+	System.out.println("The ASCII value of " + ch2 + " is: " + asciivalue2);  
+	
+	}  
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-		
-		// character whose ASCII value to be found  
-		char ch1 = 'a';  
-		char ch2 = 'b';  
-		// variable that stores the integer value of the character  
-		int asciivalue1 = ch1;  
-		int asciivalue2 = ch2;  
-		System.out.println("The ASCII value of " + ch1 + " is: " + asciivalue1);  
-		System.out.println("The ASCII value of " + ch2 + " is: " + asciivalue2);  
-		}  
-		  
-	}
-
-
+}
