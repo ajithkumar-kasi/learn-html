ce7473947e68faab696ae574306bd7441fa29b3d
AJITHKUMAR <116996938+ajithkumar-kasi@users.noreply.github.com>
2022-12-19 16:04:47 +0530

    Add files via upload

---
+++ rightshift1.java
@@ -0,0 +1,16 @@
+package mypack1;
+
+public class rightshift1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println(20>>2);
+		System.out.println(20>>>2);
+		System.out.println(-20>>2);
+		System.out.println(-20>>>2);
+		System.out.println(20>>3);
+		
+	}
+
+}
