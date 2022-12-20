86dd6b33f09a03c770331d46550a7e65993914c9
AJITHKUMAR <116996938+ajithkumar-kasi@users.noreply.github.com>
2022-12-20 12:40:20 +0530

    Add files via upload

---
+++ longtostr.java
@@ -0,0 +1,10 @@
+package mypack1;
+
+public class longtostr {
+
+		public static void main(String args[]){  
+		long i=9993939399L;  
+		String s=String.valueOf(i);  
+		System.out.println(s);  
+		}}  
+
