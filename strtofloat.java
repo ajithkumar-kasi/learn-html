267026a7c73fd4a0238b0779ebd6d6de42ee81f0
AJITHKUMAR <116996938+ajithkumar-kasi@users.noreply.github.com>
2022-12-20 12:41:18 +0530

    Add files via upload

---
+++ strtofloat.java
@@ -0,0 +1,8 @@
+package mypack1;
+
+public class strtofloat {
+		public static void main(String args[]){  
+		String s="23.6";  
+		float f=Float.parseFloat("23.6");  
+		System.out.println(f);  
+		}}  
