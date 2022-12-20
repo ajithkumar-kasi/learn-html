0d64abef7c5039fb643f5f59f093118995be2754
AJITHKUMAR <116996938+ajithkumar-kasi@users.noreply.github.com>
2022-12-20 12:38:00 +0530

    Add files via upload

---
+++ simple.java
@@ -0,0 +1,15 @@
+package mypack1;
+
+public class simple {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int a=136;
+		byte b=(byte)a;
+		System.out.println(a);
+		System.out.println(b);
+		
+	}
+
+}
