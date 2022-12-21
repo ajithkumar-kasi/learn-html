068dcec40cd35d65f9828ebd3fa18660032a21b4
AJITHKUMAR <116996938+ajithkumar-kasi@users.noreply.github.com>
2022-12-21 07:50:36 +0530

    Add files via upload

---
+++ chartostr.java
@@ -0,0 +1,22 @@
+package mypack1;
+
+public class chartostr {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		char c='A';
+		char d='B';
+		/*
+		String s=String.valueOf(c);
+		System.out.println(s);
+		String sd=String.valueOf(d);
+		System.out.println(sd);
+
+*/
+		String s=Character.toString(c);
+		System.out.println(s);
+
+	}
+
+}
