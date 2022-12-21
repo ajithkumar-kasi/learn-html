0fb3672621da4223ab44511231342bc537a830cb
AJITHKUMAR <116996938+ajithkumar-kasi@users.noreply.github.com>
2022-12-21 07:34:41 +0530

    Add files via upload

---
+++ doubletostring.java
@@ -0,0 +1,17 @@
+package mypack1;
+
+public class doubletostring {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		double d=67.986545;
+		String s=String.valueOf(d);
+		System.out.println(s);
+		
+		String sd=Double.toString(d);
+		System.out.println(sd);
+		
+	}
+
+}
