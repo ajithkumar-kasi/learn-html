dd0a33b3e1b2b2185b6db81a3993ec8c190fc439
AJITHKUMAR <116996938+ajithkumar-kasi@users.noreply.github.com>
2022-12-22 09:57:05 +0530

    Add files via upload

---
+++ ternaryoperator.java
@@ -0,0 +1,16 @@
+package mypack1;
+
+public class ternaryoperator {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int a,b;
+		a=89;
+		b=78;
+		int c=a-b;
+		int d=(c<a)?a:b;
+		System.out.println(d);
+	}
+
+}
