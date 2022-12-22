59892f12d6722f15d05e5bef68fe6fe95809b0ff
AJITHKUMAR <116996938+ajithkumar-kasi@users.noreply.github.com>
2022-12-22 10:07:07 +0530

    Add files via upload

---
+++ strtoobj.java
@@ -0,0 +1,15 @@
+package mypack1;
+
+public class strtoobj {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		String s="ajith";
+		Object o=s;
+		
+		System.out.println(o);
+		
+	}
+
+}
