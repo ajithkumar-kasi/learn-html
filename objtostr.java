bb13a54cb94956ff7f375eeb9cb20a1a81dc8ee4
AJITHKUMAR <116996938+ajithkumar-kasi@users.noreply.github.com>
2022-12-22 10:06:31 +0530

    Add files via upload

---
+++ objtostr.java
@@ -0,0 +1,15 @@
+package mypack1;
+
+public class objtostr {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		
+		aji o=new aji();
+		String s=o.toString();
+		System.out.println(s);
+	}
+
+}
