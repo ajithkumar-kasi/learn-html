0644110b6cf4cf1074b457bb2110b038185f9eed
AJITHKUMAR <116996938+ajithkumar-kasi@users.noreply.github.com>
2022-12-25 19:16:57 +0530

    Add files via upload

---
+++ gcdnumbers.java
@@ -0,0 +1,22 @@
+package mypack1;
+
+public class gcdnumbers {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int x = 12, y = 8, gcd = 1 ;  
+		//running loop form 1 to the smallest of both numbers  
+		for(int i = 1; i <= x && i <= y; i++)  
+		{  
+		//returns true if both conditions are satisfied   
+		if(x%i==0 && y%i==0)  
+		//storing the variable i in the variable gcd  
+		gcd = i;  
+		}  
+		//prints the gcd  
+		System.out.printf("GCD of %d and %d is: %d", x, y, gcd);  
+				
+	}
+
+}
