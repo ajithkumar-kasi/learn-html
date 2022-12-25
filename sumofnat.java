0fb42ff294d451ccc93fa740d56087312b384f72
AJITHKUMAR <116996938+ajithkumar-kasi@users.noreply.github.com>
2022-12-25 19:23:30 +0530

    Add files via upload

---
+++ sumofnat.java
@@ -0,0 +1,19 @@
+package mypack1;
+
+public class sumofnat {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int  num = 10, sum = 0;  
+		 
+		for(int i = 1; i <= num; ++i)  
+		{  
+		sum = sum + i;  
+		}  
+		System.out.println("Sum of First 10 Natural Numbers is = " + sum);  
+	}  
+		}  
+	
+
+
