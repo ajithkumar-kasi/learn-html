4a2cf62b46c072ab29850dee056d4ff0a3f245cd
AJITHKUMAR <116996938+ajithkumar-kasi@users.noreply.github.com>
2022-12-27 19:26:04 +0530

    Add files via upload

---
+++ deleteduplicatenumbers.java
@@ -0,0 +1,16 @@
+package mypack1;
+
+public class deleteduplicatenumbers {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int dup1[]= {1,2,3,4,2,6,3,84,1};
+		System.out.println("original array");
+		for(int i:dup1)
+			System.out.print(i+" ");
+	
+		System.out.println(" ");
+	}
+
+}
