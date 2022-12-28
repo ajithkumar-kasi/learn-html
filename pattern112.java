f44e0c55000e23896c844b43c09d35d769d900af
AJITHKUMAR <116996938+ajithkumar-kasi@users.noreply.github.com>
2022-12-28 20:16:46 +0530

    Add files via upload

---
+++ pattern112.java
@@ -0,0 +1,22 @@
+package mypack1;
+
+public class pattern112 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		  {  
+		        int n = 4;  
+		        for(int i = 0 ; i <= n ; i++)  
+		        {  
+		        for(int j = 0 ; j <= i ; j++)  
+		        {  
+		            System.out.print(" "+(char)(65 + i));  
+		        }  
+		            System.out.println("");  
+		        }  
+		    }  
+		 
+	}
+
+}
