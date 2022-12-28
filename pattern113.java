332442128c6a3dabdb5e3d70a466bda9d479c033
AJITHKUMAR <116996938+ajithkumar-kasi@users.noreply.github.com>
2022-12-28 20:19:00 +0530

    Add files via upload

---
+++ pattern113.java
@@ -0,0 +1,29 @@
+package mypack1;
+
+public class pattern113 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int size = 0;  
+	       Character c;  
+	       System.out.println();  
+	       size = 5;  
+	       int i, j, k;  
+	       for (i = 0; i < size + 1; i++)
+	       { 
+	    	   for (j = size; j > i; j--)
+	    	   {  
+	               System.out.print(" ");  
+	           }  
+	           for (k = 0; k < (2 * i - 1); k++)
+	           {  
+	               System.out.print("*");  
+	           }  
+	           System.out.println();  
+	         }  
+	   }  
+	   
+	}
+
+
