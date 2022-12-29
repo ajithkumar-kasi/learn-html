55acffa08ffddde5492e7bd03b5a9e25a2e2a252
AJITHKUMAR <116996938+ajithkumar-kasi@users.noreply.github.com>
2022-12-29 18:43:48 +0530

    Add files via upload

---
+++ binarytodec.java
@@ -0,0 +1,15 @@
+package mypack1;
+
+public class binarytodec {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+			String binaryString="1010";  
+			int decimal=Integer.parseInt(binaryString,2);  
+			System.out.println(decimal);  
+			  
+	}
+
+}
