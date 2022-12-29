a8f9be95dacd660d6f4417c482dcb427a7a16847
AJITHKUMAR <116996938+ajithkumar-kasi@users.noreply.github.com>
2022-12-29 18:55:26 +0530

    Add files via upload

---
+++ hextodecimal.java
@@ -0,0 +1,13 @@
+package mypack1;
+
+public class hextodecimal {
+
+	
+		public static void main(String args[]){  
+			String hex="a";  
+			int decimal=Integer.parseInt(hex,16);  
+			System.out.println(decimal);  
+			}
+	
+
+}
