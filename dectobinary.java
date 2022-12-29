e6819c5d85dd43708c893f58632aaeb4496b718b
AJITHKUMAR <116996938+ajithkumar-kasi@users.noreply.github.com>
2022-12-29 18:45:26 +0530

    Add files via upload

---
+++ dectobinary.java
@@ -0,0 +1,10 @@
+package mypack1;
+
+public class dectobinary {
+
+		public static void main(String args[]){  
+		System.out.println(Integer.toBinaryString(10));  
+		System.out.println(Integer.toBinaryString(21));  
+		System.out.println(Integer.toBinaryString(31));  
+		}}  
+
