aa80942aa965e755b261fee922ac6579ac165792
AJITHKUMAR <116996938+ajithkumar-kasi@users.noreply.github.com>
2022-12-30 19:36:20 +0530

    Add files via upload

---
+++ dectooct.java
@@ -0,0 +1,12 @@
+package mypack1;
+
+public class dectooct {
+
+	public class DecimalToOctalExample1{  
+		public static void main(String args[]){  
+		
+		System.out.println(Integer.toOctalString(8));  
+		System.out.println(Integer.toOctalString(19));  
+		System.out.println(Integer.toOctalString(81));  
+		}}  
+}
