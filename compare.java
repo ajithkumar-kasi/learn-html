9fdbb7d52ac1d604cc2b32399fd5b5059ca3f23b
AJITHKUMAR <116996938+ajithkumar-kasi@users.noreply.github.com>
2023-01-11 20:02:36 +0530

    Add files via upload

---
+++ compare.java
@@ -0,0 +1,36 @@
+package mypack1;
+import java.lang.Integer;
+
+public class compare {
+
+	// Java program to demonstrate working
+	// of compare() method using Integer Class
+
+
+		// driver code
+		public static void main(String args[])
+		{
+			int a = 10;
+			int b = 20;
+
+			// as 10 less than 20,
+			// Output will be a value less than zero
+			System.out.println(Integer.compare(a, b));
+
+			int x = 30;
+			int y = 30;
+
+			// as 30 equals 30,
+			// Output will be zero
+			System.out.println(Integer.compare(x, y));
+
+			int w = 15;
+			int z = 8;
+
+			// as 15 is greater than 8,
+			// Output will be a value greater than zero
+			System.out.println(Integer.compare(w, z));
+		}
+	}
+
+
