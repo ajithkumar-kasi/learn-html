6303e9d015cc1bd0bec387b979890270f66454f5
AJITHKUMAR <116996938+ajithkumar-kasi@users.noreply.github.com>
2023-01-14 21:17:35 +0530

    Add files via upload

---
+++ convertmstom.java
@@ -0,0 +1,21 @@
+package mypack1;
+import java.util.concurrent.TimeUnit;
+
+public class convertmstom {
+
+	
+	  public static void main(String[] args) {
+
+	    long milliseconds = 1000000;
+
+	    // us of toSeconds()
+	    // to convert milliseconds to minutes
+	    long seconds = TimeUnit.MILLISECONDS.toSeconds(milliseconds);
+	    System.out.println(milliseconds + " Milliseconds = " + seconds + " Seconds");
+
+	    // use of toMinutes()
+	    // to convert milliseconds to minutes
+	    long minutes = TimeUnit.MILLISECONDS.toMinutes(milliseconds);
+	    System.out.println(milliseconds + " Milliseconds = " + minutes + " Minutes");
+	  }
+	}
